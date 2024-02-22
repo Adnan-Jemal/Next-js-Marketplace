b49390faec550ea3db665f79f2c22dc08ce32a95
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2024-02-22 20:10:58 +0300

    plan the project

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Some thing Marketplace
+    <main className="">
+      This is Koralew or  ቆራልዬው Marketplace
     </main>
   );
 }
