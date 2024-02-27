72d0f4eff728b52387141c98263bcffa39ec02ad
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2024-02-27 20:31:36 +0300

    added navbar component

---
+++ components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <div className="w-full bg-primary h-24 ">
+      <nav>Navbar</nav>
+    </div>
+  );
+};
+
+export default Navbar;
