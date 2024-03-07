8023839af5975e3da9b5c74ddbc70055e1db74a7
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2024-03-07 08:08:46 +0300

    edited navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,10 +3,10 @@ import { ModeToggle } from "./darkModeToggle";
 
 const Navbar = () => {
   return (
-    <div className="w-full  h-24 ">
-      
-      <ModeToggle/>
-
+    <div className="w-full  px-4  items-center  bg-slate-500 ">
+      <div className=" m-auto h-20  flex items-center justify-center">
+        <ModeToggle />
+      </div>
     </div>
   );
 };
