caf3dd550e73c2c11532676c9469ca5da622ebb8
adn-abdu <134360306+Adnan-jemal@users.noreply.github.com>
2024-04-23 15:22:01 +0300

    theme setup

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -4,10 +4,7 @@ import { ModeToggle } from "./darkModeToggle";
 const Navbar = () => {
   return (
     <div className="w-full    items-center   ">
-      <div className=" m-auto h-16 px-4  flex items-center justify-center bg-slate-500">
-        <ModeToggle />
-      </div>
-      <div className=" m-auto h-16 border-b-2 flex items-center justify-center">
+      <div className=" m-auto h-16 border-b flex items-center justify-center">
         <ModeToggle />
       </div>
     </div>
